fb3dc7ac9555fa0ba6c53e0dc9a56e90a134a4e7
Filimon Raluca <raluca.filimon@info.uaic.ro>
2021-02-22 16:25:12 +0200

    modified request script

--- send_requests.js
+++ send_requests.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 url = "http://localhost:3001/api/image";
 async function send_requests() {
-    parallelReq = []
     for (let i = 0; i < 4; i++) {
+        parallelReq = []
         for (let j = 0; j < 2; j++) {
             parallelReq.push(
                 fetch(url)
@@ -10,21 +10,17 @@ async function send_requests() {
                         return data.json();
                     })
                     .then((res) => {
-                        console.log(res)
                         data = res.data;
-                        console.log(data);
                         return data;
                     })
                     .catch((error) => {
                         return error;
-                        console.log(error);
                     })
             )
         }
         await Promise.all(parallelReq).then(
             data=>console.log(data)
         );
-        parallelReq.splice(0, parallelReq.length);
     }
 }
 send_requests()
