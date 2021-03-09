0969c046043c62ce517211158e1aa97dd3e3ec2f
Filimon Raluca <raluca.filimon@info.uaic.ro>
2021-03-09 11:05:38 +0200

    separare pe foldere

--- Tema1/send_requests.js
+++ Tema1/send_requests.js
@@ -1,9 +1,10 @@
 const fetch = require('node-fetch');
-url = "http://localhost:3001/api/image";
+url = "http://localhost:3001/api/youtube?query=capoeira"
+// url = "http://localhost:3001/api/image";
 async function send_requests() {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 6; i++) {
         parallelReq = []
-        for (let j = 0; j < 2; j++) {
+        for (let j = 0; j < 5; j++) {
             parallelReq.push(
                 fetch(url)
                     .then((data) => {
